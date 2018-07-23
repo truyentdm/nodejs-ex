7acb6cdf088ffcfec074b65b29ce7359b82eca9c
TruyenTDM <truyentdm@gmail.com>
2018-07-24 01:47:03 +0700

    new footer

--- views/src/Service/Components/Footer.js
+++ views/src/Service/Components/Footer.js
@@ -5,6 +5,7 @@ class Footer extends Component{
 			<div id="footer">
 				<div id="footer_top">
 					<p>TDM Education</p>
+					<p>Website: <a href="http://tdmedu.asia">http://tdmedu.asia</a></p>
 					<p>Số 298 đường Cầu Diễn, quận Bắc Từ Liêm, Hà Nội</p>
 					<p>Email: truyentdm@gmail.com</p>
 				</div>
